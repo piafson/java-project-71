d7fc2ebfd243a3db2eee39e49af563d7be090f80
piafson <pavorobyov@yandex.ru>
2023-09-30 14:11:05 +0800

    step_3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,50 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    //@Parameters(index = "0", description = "The file whose checksum to calculate.")
+    //private File file;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    boolean versionHelpRequested;
+
+    @Override
+    public Integer call() throws Exception {
+
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
+
+
+
+
+
+/*
 public class App {
     public  static void main(String[] args) {
         System.out.println("Hello World!");
     }
-}
+}*/
