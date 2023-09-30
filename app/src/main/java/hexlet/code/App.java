a0b681311228cdd57eb17cbcb195c3dc21da2992
piafson <pavorobyov@yandex.ru>
2023-09-30 15:50:26 +0800

    step_4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,23 +1,14 @@
 package hexlet.code;
 
+import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
-
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
-
-    //@Parameters(index = "0", description = "The file whose checksum to calculate.")
-    //private File file;
-
     @Option(names = {"-h", "--help"}, usageHelp = true,
             description = "Show this help message and exit.")
     boolean usageHelpRequested;
@@ -26,9 +17,18 @@ class App implements Callable<Integer> {
             description = "Print version information and exit.")
     boolean versionHelpRequested;
 
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+
     @Override
     public Integer call() throws Exception {
-
         return 0;
     }
 
@@ -38,13 +38,3 @@ class App implements Callable<Integer> {
     }
 }
 
-
-
-
-
-/*
-public class App {
-    public  static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}*/
