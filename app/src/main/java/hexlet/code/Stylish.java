37be118975a23871fb44593c2b6568f6590574e9
piafson <pavorobyov@yandex.ru>
2023-10-07 10:54:38 +0800

    step_8 refactor(Stylish)

--- app/src/main/java/hexlet/code/Stylish.java
+++ app/src/main/java/hexlet/code/Stylish.java
@@ -4,88 +4,47 @@ import java.util.Map;
 public class Stylish {
     public static String formatting(Map<String, String> inp) {
         StringBuilder result = new StringBuilder("{\n");
+        final String add = "  + ";
+        final String unchanged = "    ";
+        final String del = "  - ";
         inp.entrySet().forEach(s -> {
-            String sub = " }";
             if (s.getKey()
                     .substring(s.getKey().lastIndexOf(" ") + 1)
                     .equals("unch")) {
-                if (s.getValue().contains("{")) {
-                    result.append("    " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue()
-                            .replaceAll("\"", "")
-                            .replaceAll(",", ", ")
-                            .replace(":", "=")
-                            + "\n");
-                } else {
-                    result.append("    " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue().replaceAll("\"", "").replaceAll(
-                            ",", ", ")
-                            + "\n");
-                }
+                result.append(unchanged + getFormatiingString(s.getKey(), s.getValue()));
             } else if (s.getKey()
                     .substring(s.getKey().lastIndexOf(" ") + 1)
                     .equals("1change")) {
-                if (s.getValue().contains("{")) {
-                    result.append("  - " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue()
-                            .replaceAll("\"", "")
-                            .replaceAll(",", ", ")
-                            .replace(":", "=")
-                            + "\n");
-                } else {
-                    result.append("  - " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue().replaceAll("\"", "").replaceAll(
-                            ",", ", ")
-                            + "\n");
-                }
+                result.append(del + getFormatiingString(s.getKey(), s.getValue()));
             } else if (s.getKey()
                     .substring(s.getKey().lastIndexOf(" ") + 1)
                     .equals("2change")) {
-                if (s.getValue().contains("{")) {
-                    result.append("  + " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue()
-                            .replaceAll("\"", "")
-                            .replaceAll(",", ", ")
-                            .replace(":", "=")
-                            + "\n");
-                } else {
-                    result.append("  + " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue().replaceAll("\"", "").replaceAll(
-                            ",", ", ")
-                            + "\n");
-                }
+                result.append(add + getFormatiingString(s.getKey(), s.getValue()));
             } else if (s.getKey()
                     .substring(s.getKey().lastIndexOf(" ") + 1)
                     .equals("add")) {
-                if (s.getValue().contains("{")) {
-                    result.append("  + " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue()
-                            .replaceAll("\"", "")
-                            .replaceAll(",", ", ")
-                            .replace(":", "=")
-                            + "\n");
-                } else {
-                    result.append("  + " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue().replaceAll("\"", "").replaceAll(
-                            ",", ", ")
-                            + "\n");
-                }
+                result.append(add + getFormatiingString(s.getKey(), s.getValue()));
             } else {
-                if (s.getValue().contains("{")) {
-                    result.append("  - " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue()
-                            .replaceAll("\"", "")
-                            .replaceAll(",", ", ")
-                            .replace(":", "=")
-                            + "\n");
-                } else {
-                    result.append("  - " + s.getKey().split(" ")[0] + ": "
-                            + s.getValue().replaceAll("\"", "").replaceAll(
-                            ",", ", ")
-                            + "\n");
-                }
+                result.append(del + getFormatiingString(s.getKey(), s.getValue()));
             }
         });
         return result + "}";
+    }
+    public static String getFormatiingString(String key, String value) {
+        String formattingString = "";
+        if (value.contains("{")) {
+            formattingString = key.split(" ")[0] + ": "
+                    + value
+                    .replaceAll("\"", "")
+                    .replaceAll(",", ", ")
+                    .replace(":", "=")
+                    + "\n";
+        } else {
+            formattingString = key.split(" ")[0] + ": "
+                    + value.replaceAll("\"", "").replaceAll(
+                    ",", ", ")
+                    + "\n";
+        }
+        return formattingString;
     }
 }
